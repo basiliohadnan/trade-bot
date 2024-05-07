bd59462c04f46486ad6bee00d30f3f5b00507dd4
Hadnan Basilio <basiliohadnan@gmail.com>
2024-05-07 11:24:08 -0300

    improved readability

--- main.py
+++ main.py
@@ -1,119 +1,111 @@
 import os
 import requests
-from dotenv import load_dotenv
 import schedule
 import time
 from datetime import datetime, timedelta
+import logging
 
-# Load environment variables from .env file
-load_dotenv()
+logging.basicConfig(level=logging.INFO)
+
+# Constants
+BITCOIN_API_URL = "https://api.coingecko.com/api/v3"
+BITCOIN_ID = "bitcoin"
+VS_CURRENCY = "usd"
+DAYS_HISTORY = 1
 
 initial_price = None
-previous_price = None
 day_start_price = None
 lowest_price_of_day = None
 highest_price_of_day = None
 
 def fetch_price():
-    url = f"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-    headers = {"Accept": "application/json"}
-
+    """Fetches the current Bitcoin price."""
     try:
-        response = requests.get(url, headers=headers)
+        url = f"{BITCOIN_API_URL}/simple/price?ids={BITCOIN_ID}&vs_currencies={VS_CURRENCY}"
+        response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        current_price = data['bitcoin']['usd']
-        return current_price
-    except Exception as e:
-        print(f"{datetime.utcnow()} - Error fetching BTC price: {e}")
+        return data[BITCOIN_ID][VS_CURRENCY]
+    except requests.RequestException as e:
+        logging.error(f"Failed to fetch Bitcoin price: {e}")
         return None
 
 def calculate_percentage_change(current_price, reference_price):
+    """Calculates the percentage change between two prices."""
     if reference_price is None:
         return None
-    else:
-        percentage_change = ((current_price - reference_price) / reference_price) * 100
-        return percentage_change
+    return ((current_price - reference_price) / reference_price) * 100
 
 def get_day_start_price():
-    # Calculate the start of the day in UTC time zone
-    start_of_day_utc = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
-    start_of_day_unix_timestamp = int(start_of_day_utc.timestamp())
-
-    # Fetch historical prices for the day
-    url = f"https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1"
-    headers = {"Accept": "application/json"}
-
+    """Fetches the start of day Bitcoin price."""
     try:
-        response = requests.get(url, headers=headers)
+        start_of_day_utc = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
+        start_of_day_unix_timestamp = int(start_of_day_utc.timestamp())
+        url = f"{BITCOIN_API_URL}/coins/{BITCOIN_ID}/market_chart?vs_currency={VS_CURRENCY}&days={DAYS_HISTORY}"
+        response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        
-        # Find the nearest price data point before the start of the day
-        day_start_price = None
+
+        # Initialize lowest and highest prices with the first price point
+        lowest_price = data['prices'][0][1]
+        highest_price = data['prices'][0][1]
+
+        # Iterate over the price points to find the lowest and highest prices
         for point in data['prices']:
-            timestamp = point[0] // 1000  # Convert milliseconds to seconds
-            if timestamp < start_of_day_unix_timestamp:
-                day_start_price = point[1]
-            else:
-                break
+            price = point[1]
+            if price < lowest_price:
+                lowest_price = price
+            if price > highest_price:
+                highest_price = price
         
-        return day_start_price
-    except Exception as e:
-        print(f"{datetime.utcnow()} - Error fetching start of day BTC price: {e}")
-        return None
+        return lowest_price, highest_price
+
+    except requests.RequestException as e:
+        logging.error(f"Failed to fetch start of day BTC price: {e}")
+        return None, None
 
 def job():
-    global initial_price, previous_price, day_start_price, lowest_price_of_day, highest_price_of_day
+    """Performs the main job function."""
     current_price = fetch_price()
-    
+    if current_price is None:
+        return
+
+    logging.info(f"Current Bitcoin price: ${current_price}")
+
+    global initial_price, day_start_price, lowest_price_of_day, highest_price_of_day
     if initial_price is None:
         initial_price = current_price
-    
+
     if day_start_price is None:
-        day_start_price = get_day_start_price()
+        lowest_price_of_day, highest_price_of_day = get_day_start_price()
+        day_start_price = (lowest_price_of_day + highest_price_of_day) / 2  # Use average price as the day start price
+        logging.info(f"Start of day Bitcoin price: ${day_start_price}")
     
-    if lowest_price_of_day is None or current_price < lowest_price_of_day:
+    if current_price < lowest_price_of_day:
         lowest_price_of_day = current_price
     
-    if highest_price_of_day is None or current_price > highest_price_of_day:
+    if current_price > highest_price_of_day:
         highest_price_of_day = current_price
-    
-    if current_price is not None:
-        print(f"{datetime.utcnow()} - Current Bitcoin price: ${current_price}")
-        
-        percentage_change_since_first_run = calculate_percentage_change(current_price, initial_price)
-        if percentage_change_since_first_run is not None:
-            print(f"{datetime.utcnow()} - Percentage Change since first run: {percentage_change_since_first_run:.2f}%")
-        
+
+    if day_start_price is not None:
         percentage_change_since_day_start = calculate_percentage_change(current_price, day_start_price)
-        if percentage_change_since_day_start is not None:
-            print(f"{datetime.utcnow()} - Percentage Change since day start: {percentage_change_since_day_start:.2f}%")
-    
-    # Calculate percentage difference from lowest and highest price of the day
+        logging.info(f"Percentage Change since day start: {percentage_change_since_day_start:.2f}%")
+
     if lowest_price_of_day is not None:
         percentage_diff_from_lowest = calculate_percentage_change(current_price, lowest_price_of_day)
-        print(f"{datetime.utcnow()} - Percentage Difference from Lowest Price of the Day: {percentage_diff_from_lowest:.2f}%")
-    
+        logging.info(f"Percentage Difference from Lowest Price of the Day: {percentage_diff_from_lowest:.2f}%")
+
     if highest_price_of_day is not None:
         percentage_diff_from_highest = calculate_percentage_change(current_price, highest_price_of_day)
-        print(f"{datetime.utcnow()} - Percentage Difference from Highest Price of the Day: {percentage_diff_from_highest:.2f}%")
-    
-    previous_price = current_price
+        logging.info(f"Percentage Difference from Highest Price of the Day: {percentage_diff_from_highest:.2f}%")
 
 def main(interval_minutes):
-    global day_start_price, lowest_price_of_day, highest_price_of_day
-    # Log the start day price when main is called
-    day_start_price = get_day_start_price()
-    print(f"{datetime.utcnow()} - Start of day Bitcoin price: ${day_start_price}")
-    
-    # Reset lowest and highest prices at the start of each day
-    lowest_price_of_day = day_start_price
-    highest_price_of_day = day_start_price
-    
+    """Main function to run the application."""
+    global initial_price, day_start_price, lowest_price_of_day, highest_price_of_day
+
     # Run job once immediately
     job()
-    
+
     # Schedule price fetching task at specified interval
     schedule.every(interval_minutes).minutes.do(job)
 
